0ea3aaebb4a2cd682b4e4105ded3934d12f55423
Henry <henryhparrish@gmail.com>
2024-11-22 10:55:56 -0700

    sort function impleted in harvester

--- src/scripts/harvester.js
+++ src/scripts/harvester.js
@@ -17,5 +17,53 @@ export const harvestPlants = (seedsInFieldArray) => {
             }
         }
     }
-    return harvestArray
+    // in harvestArray itterating through objects and setting them to a and b. First itteration object index 0 would be a, index 1 would be b
+    // then we use localC
+
+    // USING .localeCompare()
+    // if a's letter comes before b's letter in the alphabet, return -1 (ex. "apple".localeCompare("bannana"))
+    // if a's letter after b's letter in the alphabet, return 1 (ex. "bannana".localeCompare("apple"))
+    // if a's letter and b's letter is the same in the alphabet, return 0 (ex. "apple".localeCompare("apple"))
+    
+    // const sortedHarvestArray = harvestArray.sort(
+    //     (a, b) => a.type.localeCompare(b.type) //callback function for .sort(), returns value based on which argument comes first
+    // ) 
+    
+    // without using .localeCompare()
+    const sortedHarvestArray = harvestArray.sort(
+        (a, b) => {
+            if (a.type.toLowerCase() > b.type.toLowerCase()){
+                return 1
+            } else {
+                return -1
+            }
+        }
+    )
+    
+    return sortedHarvestArray
+
+    
+    // sort uses a callback function to determine proper order of a, b. .sort expects a number: -1, 0, 1
+    // callback function: (a, b) => a.type.localeCompare(b.type)   
+                //        ( cornObject, asparagusObject) => "Corn".localeCompare("Asparagus")
+                //                                                .localeCompare() returns a number. 
+                //                                                 Negative if a comes before b, 0 if a === b, 
+                //                                                   positive if a comes after b
+                //                                      => Corn > Asparagus 
+                //                                         returns positive number
+                // .sort(1) pushes b, a || Asparagus, Corn
+   
+    /*
+    *** arrayToBeSorted.sort() FUNCTION DEFINITION ***
+    const sort = (arrayToBeSorted, callBackfunction(a,b)) => {
+        newArray = []
+        for ([a, b] of arrayToBeSorted)
+            if (callbackFunction(a, b) <= 0 ){
+                newArray.push(a, b)
+            } else if (callbackFunction(a, b) > 0 ) {
+                newArray.push(b, a) 
+        }
+        return newArray
+    }
+    */
 } 
