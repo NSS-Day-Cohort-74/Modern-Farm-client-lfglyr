d25a9ec996f9c5413c7137aa435b02375e2f1021
Henry <henryhparrish@gmail.com>
2024-11-22 13:04:03 -0700

    implemented sort

--- src/scripts/harvester.js
+++ src/scripts/harvester.js
@@ -3,7 +3,7 @@
 //returns array of objects
 export const harvestPlants = (seedsInFieldArray) => {
     const harvestArray = []
-    // iterate through seedsInFIeldArray, get the output property value of each seedObject
+    // iterate through seedsInFieldArray, get the output property value of each seedObject
     // output property determines number of plant objects to push to harvestArray (duplicate)
     // CORN IS THE EXCEPTION -- no matter the output property, only one corn object gets added to harvestArray
 
@@ -17,53 +17,47 @@ export const harvestPlants = (seedsInFieldArray) => {
             }
         }
     }
-    // in harvestArray itterating through objects and setting them to a and b. First itteration object index 0 would be a, index 1 would be b
-    // then we use localC
 
-    // USING .localeCompare()
-    // if a's letter comes before b's letter in the alphabet, return -1 (ex. "apple".localeCompare("bannana"))
-    // if a's letter after b's letter in the alphabet, return 1 (ex. "bannana".localeCompare("apple"))
-    // if a's letter and b's letter is the same in the alphabet, return 0 (ex. "apple".localeCompare("apple"))
-    
-    // const sortedHarvestArray = harvestArray.sort(
-    //     (a, b) => a.type.localeCompare(b.type) //callback function for .sort(), returns value based on which argument comes first
-    // ) 
-    
-    // without using .localeCompare()
+    // SORTING OUTPUT CHALLENGE
+    // .sort() iterates through an array, taking two elements, comparing them to determine if they should be swapped.
+    // to determine if they should be swapped, a callback function that returns a positive or negative value is used. 
+    // if callback function returns negative, swap elements.
+
     const sortedHarvestArray = harvestArray.sort(
         (a, b) => {
-            if (a.type.toLowerCase() > b.type.toLowerCase()){
-                return 1
+            if (a.type.toLowerCase() < b.type.toLowerCase()) { // if true, a should come before b
+                return -1 // don't swap
+            } else if (a.type.toLowerCase() > b.type.toLowerCase()) { // if true, a should come after b
+                return 1 // swap
             } else {
-                return -1
+                return 0 // leave as is
             }
         }
     )
     
     return sortedHarvestArray
 
-    
     // sort uses a callback function to determine proper order of a, b. .sort expects a number: -1, 0, 1
-    // callback function: (a, b) => a.type.localeCompare(b.type)   
+
+    // callback function using localeCompare(): (a, b) => a.type.localeCompare(b.type)   
                 //        ( cornObject, asparagusObject) => "Corn".localeCompare("Asparagus")
                 //                                                .localeCompare() returns a number. 
-                //                                                 Negative if a comes before b, 0 if a === b, 
-                //                                                   positive if a comes after b
+                //                                                 Negative(falsy) if a comes before b, 0 if a === b, 
+                //                                                 positive(truthy) if a comes after b
                 //                                      => Corn > Asparagus 
                 //                                         returns positive number
-                // .sort(1) pushes b, a || Asparagus, Corn
+                // .sort()|| Asparagus, Corn
+
+    // if a's letter comes before b's letter in the alphabet, return -1 (ex. "apple".localeCompare("bannana"))
+    // if a's letter comes after b's letter in the alphabet, return 1 (ex. "bannana".localeCompare("apple"))
+    // if a's letter and b's letter is the same in the alphabet, return 0 (ex. "apple".localeCompare("apple"))
+    
+    // const sortedHarvestArray = harvestArray.sort(
+    //     (a, b) => a.type.localeCompare(b.type) //callback function for .sort(), returns value based on which argument comes first
+    // ) 
+    
+
+
    
-    /*
-    *** arrayToBeSorted.sort() FUNCTION DEFINITION ***
-    const sort = (arrayToBeSorted, callBackfunction(a,b)) => {
-        newArray = []
-        for ([a, b] of arrayToBeSorted)
-            if (callbackFunction(a, b) <= 0 ){
-                newArray.push(a, b)
-            } else if (callbackFunction(a, b) > 0 ) {
-                newArray.push(b, a) 
-        }
-        return newArray
-    }
-    */
+
 } 
